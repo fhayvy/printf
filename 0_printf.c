fa161d69ec3a8e738fc876d81ab46828929ff508
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-08-20 15:49:20 +0000

    task 0

---
+++ 0_printf.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+/**
+ * _printf - Produces output according to a format
+ * @format: A character string
+ * Return: The number of characters printed
+ */
+
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int printed = 0;
+	char ch;
+	char *sh;
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				ch = va_arg(args, int);
+				write(1, &ch, 1);
+				printed++;
+			}
+			else if (*format == 's')
+			{
+				sh = va_arg(args, char *);
+				write(1, sh, strlen(sh));
+				printed++;
+			}
+			else if (*format == '%')
+			{
+				write(1, format, 1);
+				printed++;
+			}
+		}
+		else
+		{
+			write(1, format, 1);
+			printed++;
+		}
+		format++;
+	}
+	va_end(args);
+
+	return (printed);
+}
