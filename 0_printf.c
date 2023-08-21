38cc812e741b40c1ab38fee3145ec89949b4f72d
Ayodele Olaleye <haryorolal2@gmail.com>
2023-08-21 06:33:39 -0700

    Task 0

--- 0_printf.c
+++ 0_printf.c
@@ -1,63 +1,77 @@
 #include "main.h"
-
-
+/**
+ * caseC - function to write single character
+ * @count: numbers to track character
+ * @dig: argument to store values
+ * Return: returns count
+ */
+int caseC(int count, int dig)
+{
+	write(1, &dig, 1);
+	return (count);
+}
+/**
+ * caseS - function to write string
+ * @count: numbers to tract character
+ * @dig: pointer argument to store values
+ * Return: returns count
+ */
+int caseS(int count, char *dig)
+{
+	if (dig != NULL)
+	{
+		write(1, dig, strlen(dig));
+		return (count + strlen(dig));
+	}
+	else
+	{
+		write(1, "(null)", 6);
+		return (count + 6);
+	}
+}
+/**
+ * caseMod - function to write %
+ * @count: numbers to track character
+ * Return: returns count
+ */
+int caseMod(int count)
+{
+	write(1, "%", 1);
+	return (cont + 1);
+}
 /**
  * _printf - Produces output according to a format
  * @format: A character string
  * Return: The number of characters printed
  */
-
-
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int printed = 0, i = 0, length;
-	int ch;
-	char *sh;
-
-	if (format == NULL)
-		return (-1);
-
-	if (*format == '%')
-		return (-1);
+	int dig, i = 0;
+	char *dig2;
 
 	va_start(args, format);
 
-	while (*format)
+	if (format)
 	{
-		if (*format == '%')
+		while (format[i])
 		{
-			format++;
-			if (*format == '\0')
+			if (*format[i] == '%')
 			{
-				break;
+				i++;
+				if (*format == 'c')
+					i = caseC(i, va_arg(arg, int));
+				else if (*format == 's')
+					i = caseC(i, va_arg(arg, char *));
+				else if (*format == 'c')
+					i = caseMod(i);
 			}
-			else if (*format == '%')
+			else
 			{
 				write(1, format, 1);
-				printed++;
-			}
-			else if (*format == 'c')
-			{
-				ch = va_arg(args, int);
-				write(1, &ch, 1);
-				printed++;
-			}
-			else if (*format == 's')
-			{
-				sh = va_arg(args, char *);
-				for (i = 0; sh[i] != '\0'; i++)
-					length++;
-				write(1, sh, length);
-				printed += length;
+				i++;
 			}
 		}
-		else
-		{
-			write(1, format, 1);
-			printed++;
-		}
-		format++;
 	}
 	va_end(args);
 	return (printed);
