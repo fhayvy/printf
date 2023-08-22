8ee5072993420c84b7e942a0b650f06a7fe77b30
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-08-22 09:30:35 +0000

    task 0

--- 0_printf.c
+++ 0_printf.c
@@ -1,77 +1,62 @@
-#include "main.h"
+include "main.h"
+
+
 /**
- * caseC - function to write single character
- * @count: numbers to track character
- * @dig: argument to store values
- * Return: returns count
- */
-int caseC(int count, int dig)
-{
-	write(1, &dig, 1);
-	return (count);
-}
-/**
- * caseS - function to write string
- * @count: numbers to tract character
- * @dig: pointer argument to store values
- * Return: returns count
- */
-int caseS(int count, char *dig)
-{
-	if (dig != NULL)
-	{
-		write(1, dig, strlen(dig));
-		return (count + strlen(dig));
-	}
-	else
-	{
-		write(1, "(null)", 6);
-		return (count + 6);
-	}
-}
-/**
- * caseMod - function to write %
- * @count: numbers to track character
- * Return: returns count
- */
-int caseMod(int count)
-{
-	write(1, "%", 1);
-	return (count + 1);
-}
-/**
- * _printf - Produces output according to a format
+ * _printf - Produces an output according to a format
  * @format: A character string
  * Return: The number of characters printed
  */
+
+
 int _printf(const char *format, ...)
 {
-	va_list arg;
-	int i = 0;
+	int printed = 0;
+	int ch;
+	char *sh;
 
-	va_start(arg, format);
+	va_list args_list;
 
-	if (format)
+	if (format == NULL)
 	{
-		while (format[i])
+		return (-1);
+	}
+
+	va_start(args_list, format);
+
+	while (*format)
+	{
+		if (*format != '%')
 		{
-			if (format[i] == '%')
+			write(1, format, 1);
+			printed++;
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
 			{
-				i++;
-				if (format[i] == 'c')
-					i = caseC(i, va_arg(arg, int));
-				else if (format[i] == 's')
-					i = caseS(i, va_arg(arg, char *));
-				else if (format[i] == '%')
-					i = caseMod(i);
+				break;
 			}
-			else
+			else if (*format == 'c')
 			{
-				write(1, format + i, 1);
-				i++;
+				ch = va_arg(args_list, int);
+				write(1, &ch, 1);
+				printed++;
+			}
+			else if (*format == 's')
+			{
+				sh = va_arg(args_list, char *);
+				write(1, sh, strlen(sh));
+				printed += strlen(sh);
+			}
+			else if (*format == '%')
+			{
+				write(1, format, 1);
+				printed++;
 			}
 		}
+		format++;
 	}
-	va_end(args);
-	return (i);
+	va_end(args_list);
+	return (printed);
 }
--- main.h
+++ main.h
@@ -1,12 +1,10 @@
-#ifndef _PRINTF_H_
-#define _PRINTF_H_
-
-#include <stdarg.h>
+#ifndef MAIN_H
+#define MAIN_H
 #include <unistd.h>
+#include <stdarg.h>
 #include <string.h>
 #include <stdlib.h>
-#include <stdio.h>
-#include <limits.h>
+
 
 int _printf(const char *format, ...);
 
